d0693c79ef070587040e70840124ebffea852bc1
anita <anita.ani2007@gmail.com>
2018-10-27 06:50:25 -0700

    added two components to display map and filter

---
+++ src/FilterLocation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilterLocation extends Component {
+  render() {
+    return (
+      <div className='filter-location'>
+        filter location
+      </div>
+    );
+  }
+}
+
+export default FilterLocation;
---
+++ src/ShowMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowMap extends Component {
+  render() {
+    return (
+      <div className='show-map'>
+        Showing map
+      </div>
+    );
+  }
+}
+
+export default ShowMap;
