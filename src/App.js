e7286af766e483735f0cf0a6876bae5101cecbfb
anita <anita.ani2007@gmail.com>
2018-10-27 22:02:58 -0700

    map is added to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import FilterLocation from './FilterLocation';
 import ShowMap from './ShowMap';
-import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- src/ShowMap.js
+++ src/ShowMap.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class ShowMap extends Component {
   render() {
     return (
-      <div className='show-map'>
-        Showing map
-      </div>
+      <Map google={this.props.google} initialCenter={{
+            lat: 32.576139,
+            lng: -117.014674
+          }} zoom={14}>
+      </Map>
     );
   }
 }
 
-export default ShowMap;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyBKB1JNeGT7tiLlNKdvSfnoXndV1UT8QIw")
+})(ShowMap);
